0c4758b3ec293c076979047cc63186c6fe9cb669
inari a <iaaltojarvi@gmail.com>
2019-01-29 14:47:55 +0100

    Add-nappi korjattu ja table näyttää kaikki viestit

--- public/client.js
+++ public/client.js
@@ -1,14 +1,14 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
     // cookie username to write hello username
     const cookie = document.cookie.split("=");
     const span = $('#welcome');
-      span.text(`${cookie[1]}`);
-    
-    $("#btn").click(function() {
-        
-        $.getJSON('/api/diaryEntries', function(jsondata) {
-            var $entryList = $("#list");
+    span.text(`${cookie[1]}`);
+
+    $("#btn").click(function () {
+
+        $.getJSON('/api/diaryEntries', function (jsondata) {
+            var $entryList = $("tbody");
             $entryList.empty();
             var journalItems = jsondata;
             console.log("jounnalItems pituus ", journalItems.length);
@@ -20,40 +20,47 @@ $(document).ready(function() {
                 for (var textindex in diaryEntries) {
                     var diaryText = diaryEntries[textindex].diaryText;
                     var date = diaryEntries[textindex].date;
-                    $("<li>").text(date +", " +writer +": " + diaryText).appendTo($entryList);
+                    var $tr = $("<tr>");
+                    $tr.appendTo($entryList);
+                    //   ($("<td>").text(date).$("<td>").text(writer).$("<td>").text(diaryText)).appendTo($tr);
+                    $("<td>").text(date).appendTo($tr);
+                    $("<td>").text(writer).appendTo($tr);
+                    $("<td>").text(diaryText).appendTo($tr);
+
                 }
-                
+
             }
 
         })
+
     });
 
-    $("#btn-add").click(function() {
+    $("#btn-add").click(function () {
 
-       // var $writer = $("#writer").val();
+        // var $writer = $("#writer").val();
         var writer = cookie[1];
         console.log("kirjoittaja", writer); //value from cookie
-        
+
         var $entry = $("#learned").val();
 
-        var diaryEntry = {"name": writer, "id": 22, "diaryEntry": $entry};
+        var diaryEntry = { "name": writer, "id": 22, "diaryEntry": $entry };
         //console.dir(JSON.stringify(diaryEntry));
         var settings = {
-                        "async": true,
-                        "crossDomain": true,
-                        "url": "http://localhost:3000/api/diaryEntries",
-                        "method": "POST",
-                        "headers": {
-                            "Content-Type": "application/json",
-                            "Accept": "application/json",
-                            "Cache-Control": "no-cache"
-                        },
-                        "processData": false,
-                        "data": JSON.stringify(diaryEntry)
+            "async": true,
+            "crossDomain": true,
+            "url": "http://localhost:3000/api/diaryEntries",
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Cache-Control": "no-cache"
+            },
+            "processData": false,
+            "data": JSON.stringify(diaryEntry)
         }
 
-        $.ajax(settings).done(function(response){ //$.ajax(settings) lähettää post:ina
-            console.log("postin vastaus",response);
+        $.ajax(settings).done(function (response) { //$.ajax(settings) lähettää post:ina
+            console.log("postin vastaus", response);
         });
     })
 
