a6e7f708df502f6a3b1af9d0ca2ad8e72ce3d0ca
MariV <mmk.vaananen@gmail.com>
2019-01-29 11:25:19 +0200

    Get all diary entries, Ḿari

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+var parser = bodyParser.urlencoded({extended: true});
+app.use(bodyParser.json());
+var router = express.Router();
+var reader = require('./js/fileReader')
+const fs = require('fs');
+
 
 app.use(bodyParser.urlencoded( { extended: true } ));
 app.use(express.static(__dirname + '/public'));
@@ -20,6 +26,17 @@ app.post('/goodbye', function(req, res) {
     res.redirect('/');
 });
 
+router.route('/diaryEntries')
+//get all the diary entries
+.get(function (req, res) {
+    console.dir(req.query);
+    reader.readDiaryFile('./files/data.json', function(fileContent) {
+        //console.log(fileContent);
+        res.json(fileContent);
+    });  
+})
+
+app.use('/api', router);
 
 let server = app.listen(3000, () => {
     console.log(`Server listening on ${server.address().port}`);
