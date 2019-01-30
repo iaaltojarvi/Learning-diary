90a4fb07d11a8d1cb4e4fbbfb459ee61ddad3294
MariV <mmk.vaananen@gmail.com>
2019-01-30 14:05:51 +0200

    my Listing index sivulle ja client.jssä, Mari

--- public/client.js
+++ public/client.js
@@ -38,32 +38,45 @@ $(document).ready(function () {
     $("#btn_my").click(function () {
         //Vaatii tuloksen käsittelyn tauluun
         $.getJSON('/api/diaryEntries/' + cookie[1], function (jsondata) {
-            var $entryList = $("tbody");
+            var $entryList = $("#accordion");
             $entryList.empty();
             var journalItems = jsondata;
-            // console.log("haetaan userin entryt ", cookie[1]);
+            console.log("users entries data: ", journalItems);
+            console.log("haetaan userin entryt ", cookie[1]);
             for (var index in journalItems) {
-                var writer = journalItems[index].name;
-                var id = journalItems[index].id;
-                var diaryEntries = journalItems[index].diaryItemList;
-                // console.log(diaryEntries.length);
-                for (var textindex in diaryEntries) {
-                    var diaryText = diaryEntries[textindex].diaryText;
-                    var date = diaryEntries[textindex].date;
-                    var $tr = $("<tr>");
-                    $tr.appendTo($entryList);
-                    //   ($("<td>").text(date).$("<td>").text(writer).$("<td>").text(diaryText)).appendTo($tr);
-                    $("<td>").text(date).appendTo($tr);
-                    $("<td>").text(writer).appendTo($tr);
-                    $("<td>").text(diaryText).appendTo($tr);
+                var writer = cookie[1];
+                var textId = journalItems[index].textID;
+                var diaryText = journalItems[index].diaryText;
+                var date = journalItems[index].date;
+                
 
-                }
+                //console.log("tulostetaan rivia", index);
+                
+                //entry message preview
+                $entryList.append($("<div>").addClass("panel panel-default").append($("<div>")
+                .addClass("panel-heading").attr("id", "heading" +index).attr("role", "tab").append($("<h4>")
+                .addClass("panel-title").append($("<div>")
+                .addClass("collapsed").attr("role", "button")
+                .attr("data-toggle", "collapse").attr("data-parent", "#accordion").attr("href", "#collapse" +index)
+                .attr("aria-expanded", "false").attr("aria-controls", "collapse"+index)
+                .text(date + " " + diaryText)))));
 
+                $entryList.append($("<div>").addClass("panel-collapse collapse")
+                .attr("id", "collapse" +index).attr("role", "tabpanel")
+                .attr("aria-labelledby", "heading" + index).append($("<div>").addClass("panel-body").text(diaryText)));
+                
+                
             }
 
         })
 
     });
+
+     /* $entryList.append($("<div>").addClass("panel panel-default").append()); //
+                    $("<div>").addClass("panel-heading").attr("id", "heading" +textindex).attr("role", "tab").appendTo($entryList);
+                    $("<h4>").addClass("panel-title").appendTo($entryList);
+                    $("<div>").addClass("collapsed").attr("role", "button").attr("data-toggle", "collapse").attr("data-parent", "#accordion").attr("href", "#collapse" +textindex).attr("aria-expanded", "false").attr("aria-controls", "collapse"+textindex)
+                    .text(date + " " + diaryText).appendTo($entryList);*/
 
     $("#btn-add").click(function () {
 
@@ -99,7 +112,7 @@ $(document).ready(function () {
         //Vaatii tuloksen käsittelyn tauluun
         var username;
         var textId;
-        var params ="http://localhost:3000/api/diaryEntries/"+username + "/" + textId;
+        var params ="http://localhost:3000/api/diaryEntries/"+ username + "/" + textId;
         var settings = {
             "async": true,
             "crossDomain": true,
